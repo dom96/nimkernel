1a31de71a4dcfc50bef7b3faa207e019fb5ad89b
def <dennis@felsin9.de>
2015-05-05 17:48:21 +0200

    Minor improvements

--- main.nim
+++ main.nim
@@ -6,14 +6,14 @@ type
 proc kmain(mb_header: PMultiboot_header, magic: int) {.exportc.} =
   if magic != 0x2BADB002:
     discard # Something went wrong?
-  
+
   var vram = cast[PVIDMem](0xB8000)
   screenClear(vram, Yellow) # Make the screen yellow.
-  
+
   # Demonstration of error handling.
   var x = len(vram[])
   var outOfBounds = vram[x]
-  
+
   let attr = makeColor(Yellow, DarkGrey)
   writeString(vram, "Nim", attr, (25, 9))
   writeString(vram, "Expressive. Efficient. Elegant.", attr, (25, 10))
